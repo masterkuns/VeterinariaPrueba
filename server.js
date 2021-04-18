f79ee93ce4f236587b0db670d2afc60693530e26
yaca <angeldav99@hotmail.com>
2021-04-18 18:51:51 -0500

    cors

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+
+
+// add this code
+const whitelist = ['http://localhost:3000']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/VeterinariaPrueba'));
