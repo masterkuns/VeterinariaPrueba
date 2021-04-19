ec412e30e2b36753ebab5c57a226047cc4ff43aa
yaca <angeldav99@hotmail.com>
2021-04-18 19:18:53 -0500

    q

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+
 
 const app = express();
-
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-// add this code
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/VeterinariaPrueba'));
